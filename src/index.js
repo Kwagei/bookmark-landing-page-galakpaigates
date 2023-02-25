9dfc067f10defdad6b56bf3886461a10893d883d
galakpaigates <jeedoarkoi2006@gmail.com>
2023-02-25 08:13:23 -0500

    Revised the Phone and Computer View (Tablet View = null).

--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
-document.querySelector('#answer1DownArrow').onclick = function(){
-    let answer1Text = document.querySelectorAll(".answer1Text")
+document.addEventListener("DOMContentLoaded", function openClose(){
+    document.querySelector("#question1DownArrow").onclick = function displayAnswer1(){
+      if (document.querySelector('.answer1Text').style.display == "none") {
+        document.querySelector('.answer1Text').style.display = "list-item"
+      }
 
-    if (answer1Text.style.display == "none") {
-        answer1Text.style.display = "list-item"
+      else {
+        document.querySelector(".answer1Text").style.display = "none"
+      }
     }
-};
+
+    document.querySelector("#question2DownArrow").onclick = function displayAnswer2(){
+      if (document.querySelector('#answer2Text').style.display == "none") {
+        document.querySelector('#answer2Text').style.display = "list-item"
+      }
+
+      else {
+        document.querySelector("#answer2Text").style.display = "none"
+      }
+    }
+
+    document.querySelector("#question3DownArrow").onclick = function displayAnswer3(){
+      if (document.querySelector('#answer3Text').style.display == "none") {
+        document.querySelector('#answer3Text').style.display = "list-item"
+      }
+
+      else {
+        document.querySelector("#answer3Text").style.display = "none"
+      }
+    }
+
+    document.querySelector("#question4DownArrow").onclick = function displayAnswer4(){
+      if (document.querySelector('#answer4Text').style.display == "none") {
+        document.querySelector('#answer4Text').style.display = "list-item"
+      }
+
+      else {
+        document.querySelector("#answer4Text").style.display = "none"
+      }
+    }
+
+    document.querySelector(".displayOtherContentSvg").onclick = function displayOtherContent(){
+      if (document.querySelector('.otherContentList').style.display == "none") {
+        document.querySelector('.otherContentList').style.display = "list-item"
+      }
+
+      else {
+        document.querySelector(".otherContentList").style.display = "none"
+      }
+    }
+
+    var linkArea = document.querySelector('.linkArea');
+    
+    if (window.innerWidth > "400.1px") {
+      html('head').innerHTML == "<link rel='stylesheet' href='./src/computer.js'>"
+    }
+  });
